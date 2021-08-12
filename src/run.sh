e5be4303791ba10e3afa688f7bfc471c2a672b87
Mark McCans <mjmccans@gmail.com>
2021-08-12 12:09:51 -0400

    Playing around with configuration options.

--- src/run.sh
+++ src/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bashio
 set -e
 
-bashio::log.info "Creating DHCP configuration..."
+bashio::log.info "Getting mqtt configuration..."
 
 MQTT_HOST=$(bashio::services mqtt "host")
 MQTT_USER=$(bashio::services mqtt "username")
@@ -10,3 +10,11 @@ MQTT_PASSWORD=$(bashio::services mqtt "password")
 echo "MQTT Host: $MQTT_HOST"
 echo "MQTT Username: $MQTT_USER"
 echo "MQTT Password: $MQTT_PASSWORD"
+
+echo "MAC: $(bashio::config 'mac')"
+
+# see example: https://github.com/home-assistant/addons/edit/master/dhcp_server/data/run.sh
+for device in $(bashio::config 'devices|keys'); do
+    BROADCAST=$(bashio::config "devices[${device}].mac")
+    GATEWAY=$(bashio::config "devices[${device}].device_name")
+done
