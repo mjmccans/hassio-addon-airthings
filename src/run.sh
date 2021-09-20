fb2c99f87d77d1dcef989f5b837d327f04f88720
Mark McCans <mjmccans@gmail.com>
2021-09-20 03:42:13 +0100

    Remove restart of addon as it was not needed.

--- src/run.sh
+++ src/run.sh
@@ -9,12 +9,4 @@ MQTT_PASSWORD=$(bashio::services mqtt "password")
 bashio::log.info "Starting python script..."
 python3 ./airthings-mqtt.ha.py --host $MQTT_HOST --username $MQTT_USER --password $MQTT_PASSWORD --config /data/options.json
 
-# If the script exits with an error restart the addon.
-retVal=$?
-if [ $retVal -ne 0 ]; then
-    bashio::log.info "Waiting 30 seconds and then restarting addon because python script has exited with an error..."
-    sleep 30
-    bashio::addons.restart
-fi
-
 bashio::log.info "Done."
