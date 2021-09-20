fd530fa1f3ee9e74e26f3a2c26048112db70d7ee
Mark McCans <mjmccans@gmail.com>
2021-09-20 02:53:12 +0100

    Adding 30 second delay before restarting.

--- src/run.sh
+++ src/run.sh
@@ -12,7 +12,8 @@ python3 ./airthings-mqtt.ha.py --host $MQTT_HOST --username $MQTT_USER --passwor
 # If the script exits with an error restart the addon.
 retVal=$?
 if [ $retVal -ne 0 ]; then
-    bashio::log.info "Restarting addon because python script has exited with an error..."
+    bashio::log.info "Waiting 30 seconds and then restarting addon because python script has exited with an error..."
+    sleep 30
     bashio::addons.restart
 fi
 
