96890f80f2dae2628c0ea56de5417f9daa8ce1a2
Mark McCans <mjmccans@gmail.com>
2022-05-13 13:26:00 -0400

    Revert "Reconsile with non-addon script code."
    This reverts commit 409d2d1c1275f5b513063deace764afbdd5bb8b5.

--- src/run.sh
+++ src/run.sh
@@ -1,28 +1,16 @@
 #!/usr/bin/env bashio
 set -e
 
-MQTT_HOST=$(bashio::config 'mqtt_host')
-if [[ -z "$MQTT_HOST" || $MQTT_HOST == "null" ]]; then 
-    if ! bashio::services.available "mqtt"; then
-        bashio::log.error "No internal MQTT service found. Please install the internal Mosquitto MQTT broker integration."
-    else
-        bashio::log.info "Internal MQTT service found, fetching configuration ..."
-        MQTT_HOST=$(bashio::services mqtt "host")
-        MQTT_USER=$(bashio::services mqtt "username")
-        MQTT_PASSWORD=$(bashio::services mqtt "password")
-
-        bashio::log.info "Starting python script..."
-        python3 ./airthings-mqtt.ha.py      \
-            --mqtt_host $MQTT_HOST          \
-            --mqtt_username $MQTT_USER      \
-            --mqtt_password $MQTT_PASSWORD  \
-            --config /data/options.json     \
-            --addon
-    fi
+if ! bashio::services.available "mqtt"; then
+    bashio::log.error "No internal MQTT service found. Please install the internal Mosquitto MQTT broker integration."
 else
-    bashio::log.info "Starting python script with external MQTT broker..."
-    python3 ./airthings-mqtt.ha.py      \
-        --config /data/options.json     \
-        --addon
+    bashio::log.info "Internal MQTT service found, fetching configuration ..."
+    MQTT_HOST=$(bashio::services mqtt "host")
+    MQTT_USER=$(bashio::services mqtt "username")
+    MQTT_PASSWORD=$(bashio::services mqtt "password")
+
+    bashio::log.info "Starting python script..."
+    python3 ./airthings-mqtt.ha.py --host $MQTT_HOST --username $MQTT_USER --password $MQTT_PASSWORD --config /data/options.json
 fi
+
 bashio::log.info "Done."
